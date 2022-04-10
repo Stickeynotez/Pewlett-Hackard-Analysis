24a88e2a12443febc6516864e05479da48d13731
Stickeynotez <stickeynotez99@gmail.com>
2022-04-10 19:48:47 -0400

    updating queries

--- Queries/Employee_Database_challenge.sql.sql
+++ Queries/Employee_Database_challenge.sql.sql
@@ -37,7 +37,7 @@ ORDER BY COUNT DESC;
 
 SELECT DISTINCT ON (e.emp_no) e.emp_no, e.first_name, e.last_name, e.birth_date, de.from_date, de.to_date,
 	t.title
--- INTO mentorship_eligibility
+INTO mentorship_eligibility
 FROM employees AS e
 INNER JOIN department_employees AS de
 ON (e.emp_no = de.emp_no)
@@ -45,5 +45,3 @@ INNER JOIN titles AS t
 ON (e.emp_no = t.emp_no)
 WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
 AND (de.to_date = '9999-01-01')
-	
-select* from employees;
